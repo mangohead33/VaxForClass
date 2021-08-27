30908427a0b44bc71a3d6716966158cd50e49d46
Calvin Chau <cachau33@gmail.com>
2021-08-26 22:26:24 -0700

    Updated server to handle get request for /cdc

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
 const express = require('express');
 const path = require('path');
+const cdc = require('./cdcNews');
 
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../', 'client', 'dist')));
+
+app.get('/cdc', (req, res) => {
+  cdc.getNews()
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status(404).send(err);
+    });
+});
 
 const port = 3000;
 app.listen(port, () => {
