ef1eea5c080d1803d951a5e118f4d11440628810
Calvin Chau <cachau33@gmail.com>
2021-08-27 03:26:58 -0700

    User can now navigate through news or teacher portal

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,23 +1,43 @@
-/* eslint-disable react/no-unused-state */
+/* eslint-disable no-console */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
+import Banner from './components/Banner';
 import CovidInfo from './components/CovidInfo';
+import Teachers from './components/Teachers';
 
 class App extends React.Component {
   constructor(prop) {
     super(prop);
     this.state = {
-      value: 'fillin',
+      display: 'covidInfo',
     };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(value) {
+    this.setState({
+      display: value,
+    });
   }
 
   render() {
+    const { display } = this.state;
     return (
-      // <div className="leftbar">
-      //  Have at Least two Links
-      // </div>
-      <div className="mainPage">
-        <CovidInfo />
-      </div>
+      <>
+        <Banner />
+        <ul>
+          <li onClick={() => { this.onClick('covidInfo'); }}>CDC Covid News</li>
+          <li onClick={() => { this.onClick('teachersPortal'); }}>Teachers Portal</li>
+        </ul>
+        <div className="mainPage">
+          {display === 'covidInfo'
+        && <CovidInfo />}
+
+          {display === 'teachersPortal'
+        && <Teachers />}
+        </div>
+      </>
     );
   }
 }
