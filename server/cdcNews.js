779b28be23710083bc9a185dd06810c6f0c34f60
Calvin Chau <cachau33@gmail.com>
2021-08-26 23:38:13 -0700

    Modified baseURL to ensure only Covid-19 articles appear

--- server/cdcNews.js
+++ server/cdcNews.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const baseURL = 'https://tools.cdc.gov/api/v2/resources/media';
+const baseURL = 'https://tools.cdc.gov/api/v2/resources/media?nameContains=Covid-19';
 
 module.exports = {
   getNews: () => {
