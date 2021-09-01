c57d6b2e71706e729c6603cdb2686f666f804505
Calvin Chau <cachau33@gmail.com>
2021-08-31 18:52:32 -0700

    Fleshed out student roster display

--- client/src/components/Teachers.jsx
+++ client/src/components/Teachers.jsx
@@ -1,25 +1,64 @@
+/* eslint-disable no-console */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/no-unused-state */
 import React from 'react';
+import axios from 'axios';
+import MyClass from './MyClass';
 
 class Teachers extends React.Component {
   constructor(prop) {
     super(prop);
     this.state = {
-      dummy: 'mummy',
+      displayClass: false,
+      classData: [],
     };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get('/math')
+      .then((data) => {
+        this.setState({
+          classData: data.data,
+        });
+      })
+      .catch((err) => {
+        console.log('Error in handling request: ', err);
+      });
+  }
+
+  onClick() {
+    this.setState({
+      displayClass: true,
+    });
   }
 
   render() {
+    const { displayClass, classData } = this.state;
+    console.log(classData, ' empty');
     return (
-      <div className="teacherContainer">
-        <div className="teachersRow1">
-          <div className="teacherProfile">Mr.Dingle</div>
-          <div className="teacherProfile">Mrs.Bang</div>
-        </div>
-        <div className="teachersRow2">
-          <div className="teacherProfile">Mr.Dope</div>
-          <div className="teacherProfile">Mrs.Chit</div>
-        </div>
-      </div>
+      <>
+        {
+!displayClass
+&& (
+<div className="teacherContainer">
+  <div className="teachersRow1">
+    <div className="teacherProfile" onClick={this.onClick}>Mr.Dingle</div>
+    <div className="teacherProfile">Mrs.Bang</div>
+  </div>
+  <div className="teachersRow2">
+    <div className="teacherProfile">Mr.Dope</div>
+    <div className="teacherProfile">Mrs.Chit</div>
+  </div>
+</div>
+)
+      }
+        {
+  displayClass
+  && <MyClass classData={classData} />
+}
+      </>
     );
   }
 }
